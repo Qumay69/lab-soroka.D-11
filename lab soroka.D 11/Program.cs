85cf696cff035e2d6dc2b95faabdd253951fb8fd
Sorok <Sorok@DESKTOP-C9GGTD7>
2023-11-16 15:50:12 +0200

    Добавьте файлы проекта.

---
+++ lab soroka.D 11/Program.cs
@@ -0,0 +1,21 @@
+﻿        Console.Write("Введите текст:");
+        string input = Console.ReadLine();
+        
+        string[] words = input.Split(' ');
+
+        if (words.Length > 1)
+        {
+            string firstWord = words[0];
+            string lastWord = words[words.Length - 1];
+
+            words[0] = lastWord;
+            words[words.Length - 1] = firstWord;
+
+            string result = string.Join(" ", words);
+
+            Console.WriteLine(result);
+        }
+        else
+        {
+            Console.WriteLine(input);
+        }
